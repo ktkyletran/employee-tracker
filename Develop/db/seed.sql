a70dc9284277d9a7192ae83dac9361665f617632
Vanity <ktkyletran@gmail.com>
2021-03-27 19:21:56 -0500

    moved sql files to db folder

---
+++ Develop/db/seed.sql
@@ -0,0 +1,8 @@
+USE employeeDb;
+
+INSERT INTO department (name) values ('Example');
+
+INSERT INTO employee (firstName, lastName, role_id, manager_id) values ('Bobby', 'Bob', 24, 1);
+
+INSERT INTO role (title, salary, department_id) values ('Fake Employee', 80000, 1);
+
